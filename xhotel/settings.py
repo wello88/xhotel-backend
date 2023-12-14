401a37a0fa516dc314c983ea958af7b6e80c878b
abdow8896 <116099341+abdow8896@users.noreply.github.com>
2023-12-14 22:13:41 +0200

    upd

--- xhotel/settings.py
+++ xhotel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-97=wl=xs*0^zo8=p6=kpnq&)d$(r+1_)z9%d4k(%!##*jn8*a$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
