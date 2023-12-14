a3bc028d82c00583e0382e2d47e2d1b3d7def2d3
abdow8896 <116099341+abdow8896@users.noreply.github.com>
2023-12-15 00:52:01 +0200

    set.js

--- static_root/rest_framework/js/default.js
+++ static_root/rest_framework/js/default.js
@@ -44,4 +44,35 @@ $(document).ready(function() {
   $(window).on('load', function() {
     $('#errorModal').modal('show');
   });
+
+
+
+
+
+// Replace 'your_csrf_token' with the actual CSRF token you obtained from cookies
+const csrfToken = '4kBdldPUYrRLf87CUNCBt4vkZdQEM3Wpwjbxj9DjmwpEuLq7zhdPjCBvuJrek8iY';
+
+// Example: Make a request with the CSRF token
+fetch('/api/some-endpoint/', {
+  method: 'GET',  // Or any other HTTP method
+  headers: {
+    'Content-Type': 'application/json',
+    'X-CSRFToken': csrfToken,
+  },
+})
+  .then(response => response.json())
+  .then(data => console.log('Response Data:', data))
+  .catch(error => console.error('Error:', error));
+
+
+
+
+
+
+
+
+
+
+
+
 });
