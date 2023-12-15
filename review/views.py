23997c27edf4d685a10a4be70f32cfe3b22ba883
abdow8896 <116099341+abdow8896@users.noreply.github.com>
2023-12-15 19:26:48 +0200

    review

--- review/views.py
+++ review/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Review
 from .serializers import ReviewSerializer
-
+from rest_framework.response import Response
+from rest_framework import status
     
 class ReviewList(generics.ListCreateAPIView):
     queryset = Review.objects.all()
@@ -14,6 +15,11 @@ class ReviewList(generics.ListCreateAPIView):
         # Set the user of the review to the currently logged-in user (if applicable)
         serializer.save(user=self.request.user)
 
-    def get_queryset(self):
-        # Customize the queryset based on your requirements (optional)
-        return Review.objects.all()
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        data = {'reviews': serializer.data}
+        return Response(data, status=status.HTTP_200_OK) 
+
+    # def get_queryset(self):
+    #     return Review.objects.all()
